76805e9b8dd8225e846e4d0c6521ef2f977e7b61
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-04-10 06:18:23 -0300

    Estilos Prontos

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Text, TextInput, View, Button } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import styles from "./style";
 import ResultImc from "./ResultImc/";
 
 export default function Form() {
@@ -28,25 +29,33 @@ export default function Form() {
   }
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
           placeholder="Ex, 1.75"
           keyboardType="numeric"
         />
 
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder="Ex. 75.365"
           keyboardType="numeric"
         />
-
-        <Button onPress={() => validationImc()} title={textButton} />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => {
+            validationImc();
+          }}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc} />
     </View>
