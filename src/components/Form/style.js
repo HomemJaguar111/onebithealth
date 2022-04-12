57c91301e73eb9bc43c44a6dc3113c96ccbcfa85
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-04-11 22:02:05 -0300

    Aula 5 concluída

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -55,6 +55,20 @@ const styles = StyleSheet.create({
     height: "50%",
     alignItems: "center",
   },
+  listImcs: {
+    marginTop: 20,
+  },
+  resultImcItem: {
+    fontSize: 26,
+    color: "red",
+    height: 50,
+    width: "100%",
+    paddingRight: 20,
+  },
+  textResultItemList: {
+    fontSize: 16,
+    color: "red",
+  },
 });
 
 export default styles;
